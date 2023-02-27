260ad1efc29764fb224ca988f1877fe5335caafd
nemoxzero <63057370+nemoxzero@users.noreply.github.com>
2023-02-27 11:50:11 -0300

    Add files via upload

---
+++ src/components/AboutContainer.jsx
@@ -0,0 +1,15 @@
+const AboutContainer = () => {
+  return (
+    <section className="about-container">
+      <h2>Sobre</h2>
+      <p>
+      Sou formado no Ensino Superior em Análise e desenvolvimento de sistemas, na Instituição de Ensino UNINOVE, Trabalho na Empresa Inovacode na área de atendimento e Impressão de etiquetas RFID e tenho experiência na área de logística.
+      </p>
+      <p>
+      Afixionado por novas tecnologias e linguagem na área de Ti Acredito que apresento as qualificações necessárias para participar trabalhar na sua empresa e, desde já, coloco-me à disposição para uma entrevista, podendo assim detalhar melhor o meu perfil profissional. 
+      </p>
+    </section>
+  );
+};
+
+export default AboutContainer;
---
+++ src/components/ProjectsContainer.jsx
@@ -0,0 +1,16 @@
+const ProjectsContainer = () => {
+  return (
+    <section className="projects-container">
+      <h2>Projetos</h2>
+      <p>
+        Aqui esta alguns projetos desenvolvidos inclusive esse aqui tudo
+        no Github.
+      </p>
+      <a href="https://github.com/nemoxzero" className="btn">
+        Ver Projetos
+      </a>
+    </section>
+  );
+};
+
+export default ProjectsContainer;
